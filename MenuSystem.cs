313a16ef4375b719b67693b027f2373a5403c324
George <aeliusdev@users.noreply.github.com>
2016-01-11 09:14:39 +0000

    Added AddProductToMenu method

---
+++ MenuSystem.cs
@@ -0,0 +1,20 @@
+public class MenuSystem 
+{
+	public List<Product> products = new List<Product>();
+	
+	public MenuSystem ()
+	{
+		
+	}
+	
+	public AddProductToMenu (Product product)
+	{
+		products.Add(product);
+	}
+	
+	public RemoveProductFromMenu (Product product)
+	{
+		products.Add(product);
+	}	
+	
+}
